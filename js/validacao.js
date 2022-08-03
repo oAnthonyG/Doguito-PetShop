3e6b604e42c4c8b10e0febb8acb584256f755880
GPC <anthonytguilherme@hotmail.com>
2022-08-03 15:17:41 -0300

    add a new mensage of error for the CEP case invalidity

--- js/validacao.js
+++ js/validacao.js
@@ -42,13 +42,24 @@ export function valida(input) {
         },
         cep: {
             valueMissing: 'O campo de CEP não pode estar vazio.',
-            patternMismatch: 'O CEP digitado não é valido.'
+            patternMismatch: 'O CEP digitado não é valido.',
+            customError: 'Não foi possível buscar o CEP.'
+        },
+        logradouro: {
+            valueMissing: 'O campo de Logradouro não pode estar vazio.'
+        },
+        cidade: {
+            valueMissing: 'O campo de Cidade não pode estar vazio.'
+        },
+        estado: {
+            valueMissing: 'O campo de Estado não pode estar vazio.'
         }
     }
 
 const validadores = {
     dataNascimento:input => validaDataNascimento(input),
-    cpf:input => validaCPF(input)
+    cpf:input => validaCPF(input),
+    cep:input => recuperarCep(input)
 }
 
 function mostraMensagemDeErro(tipoDeInput, input){
@@ -143,4 +154,29 @@ function chegaDigitoVerificador(cpf, multiplicador){
 
 function confirmaDigito(soma){
     return 11 - (soma % 11)
+}
+
+function recuperarCep(input){
+    const cep = input.value.replace(/\D/g, '')
+    const url = `https://viacep.com.br/ws/${cep}/json`
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'content-type': 'application/json;charset=utf-8' 
+        }
+    }
+    if(!input.validity.patternMismatch && !input.validity.valueMissing){
+        fetch(url,options).then(
+            response => response.json()
+        ).then(
+            data =>{
+                if(data.erro){
+                    input.setCustomValidity('Não foi possível buscar o CEP.')
+                    return
+                }
+                input.setCustomValidity('')
+            }
+        )
+    }
 }
